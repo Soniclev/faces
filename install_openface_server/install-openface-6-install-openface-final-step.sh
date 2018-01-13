ebda9e7050a7e2cfa15c62ae25da2becc02f45cd
Soniclev <soniclev9@gmail.com>
2018-01-13 11:47:22 +0300

    Исправлен патчинг openface. Ускорен поиск лиц

--- install_openface_server/install-openface-6-install-openface-final-step.sh
+++ install_openface_server/install-openface-6-install-openface-final-step.sh
@@ -2,6 +2,7 @@ mkdir -p Programs
 cd Programs
 git clone https://github.com/cmusatyalab/openface.git ./openface --recursive
 cd ./openface
+find . -name "openface_server.lua" -exec sed -i "s/net = torch.load(opt.model)/net = torch.load(opt.model, \'b64\')"/g {} \;
+find . -name "align_dlib.py" -exec sed -i "s/return self.detector(rgbImg, 1)/return self.detector(rgbImg, 0)"/g {} \;
 sudo python2 setup.py install
 bash models/get-models.sh
-find . -name "openface_server.lua" -exec sed -i "s/net = torch.load(opt.model)/net = torch.load(opt.model, \'b64\')"/g {} \;
