a8dcfb55a6057ac0aa4d19d25a624981ea66c12c
Viktor Kozlov <soniclev@mail.ru>
2018-01-15 16:24:39 +0300

    Поправил скрипт установки (забыл установку sudo)
    Забыл ту самую первую команду:
    su
    apt-get isntall sudo

--- install_openface_server/install-openface-0-install-major-dependencies.sh
+++ install_openface_server/install-openface-0-install-major-dependencies.sh
@@ -1,3 +1,5 @@
 echo "deb http://ftp.by.debian.org/debian/ jessie main" >> /etc/apt/sources.list
 sudo apt-get update
+su
+apt-get install sudo -y
 sudo apt-get install python-pip libfreetype6-dev libpng12-dev pkg-config -y
